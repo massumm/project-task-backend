fce901748562dfbbc3dac368269ad36a07b46b4b
Masum Al Reza <masum321987@gmail.com>
2026-02-25 22:20:47 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.core.database import Base, engine
-from app.routes import auth,project,task, payment,admin
-
+from app.routes import auth, project, task, payment, admin
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(project.router)
 app.include_router(auth.router)
 app.include_router(task.router)
 app.include_router(payment.router)
 app.include_router(admin.router)
-
-@app.get("/")
-def root():
-    return {"message": "API is running successfully"}
